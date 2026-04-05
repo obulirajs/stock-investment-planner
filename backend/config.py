e52a2eea08899a9f890a5e29486b4539de73c9d1
Raj <obuliraj.s@gmail.com>
2026-04-05 15:51:54 +0530

    Updated specs and configurations - migrate specs to ai/specs, update config

--- backend/config.py
+++ backend/config.py
@@ -7,9 +7,11 @@
 # DB_NAME = "stock_research_db"
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # MARKETSTACK_KEY = os.getenv("MARKETSTACK_KEY", "")
-MARKETSTACK_KEY = os.getenv("MARKETSTACK_KEY", "")
-MARKETSTACK_BASE = os.getenv("MARKETSTACK_BASE", "http://api.marketstack.com/v1")
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-DB_NAME = os.getenv("DB_NAME", "stock_research_db")
+MARKETSTACK_KEY = os.getenv("MARKETSTACK_API_KEY") #, "83707bd0d216acff33c2a4f4e9dfb61e")
+MARKETSTACK_BASE = os.getenv("MARKETSTACK_BASE") #, "http://api.marketstack.com/v1")
+MONGO_URL = os.getenv("MONGO_URL") #, "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME") #, "stock_research_db")
